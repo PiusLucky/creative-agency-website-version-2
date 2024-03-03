735884c60c5c15b00f8dd3f3617341e882652c3b
PiusLucky <luckypius50@gmail.com>
2024-03-03 15:51:11 +0100

    chore: added margin to components wrapper - homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
       <div>
         <NavBar />
         <HeroSection />
-        <div className="px-4 md:px-16 flex flex-col gap-16 md:gap-32">
+        <div className="px-4 md:px-16 flex flex-col gap-16 md:gap-32 mt-16 md:mt-32">
           <AboutUsSection />
           <ServicesSection />
           <PortfolioSection />
